56b621506bc2736a3a1102d23a948e1210ca2201
Binoy <binoypatel14@gmail.com>
2023-03-13 00:00:04 -0400

    Remove Babel
    - Use SWC for jest

--- apps/home/jest.config.ts
+++ apps/home/jest.config.ts
@@ -4,7 +4,7 @@ export default {
   preset: "../../jest.preset.js",
   transform: {
     "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
-    "^.+\\.[tj]sx?$": "babel-jest",
+    "^.+\\.[tj]sx?$": "@swc/jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "../../coverage/apps/home",
