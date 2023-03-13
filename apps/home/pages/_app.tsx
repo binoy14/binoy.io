b2506606f9c4941ab3882b2eba6675d64d0d7f49
Binoy Patel <me@binoy.io>
2023-03-12 23:12:43 -0400

    Add Vercel Analytics (#29)

--- apps/home/pages/_app.tsx
+++ apps/home/pages/_app.tsx
@@ -1,30 +1,35 @@
 import "../public/global.css";
 import "../public/prism.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { NextSeo } from "next-seo";
-import { AppProps } from "next/app";
 
 import { Layout } from "../components/Layout";
 
+import type { AppProps } from "next/app";
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <NextSeo
-        title="Binoy Patel"
-        description="Tangential is a consulting company that helps companies with Web and Mobile Applications."
-        canonical="https://binoy.io"
-        openGraph={{
-          description: "Tangential is a consulting company that helps companies with Web and Mobile Applications.",
-          title: "Binoy Patel",
-          url: "https://binoy.io",
-          type: "website",
-          site_name: "Binoy Patel",
-        }}
-      />
-      <div>
-        <Component {...pageProps} />
-      </div>
-    </Layout>
+    <>
+      <Layout>
+        <NextSeo
+          title="Binoy Patel"
+          description="Tangential is a consulting company that helps companies with Web and Mobile Applications."
+          canonical="https://binoy.io"
+          openGraph={{
+            description: "Tangential is a consulting company that helps companies with Web and Mobile Applications.",
+            title: "Binoy Patel",
+            url: "https://binoy.io",
+            type: "website",
+            site_name: "Binoy Patel",
+          }}
+        />
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+      <Analytics />
+    </>
   );
 }
 
