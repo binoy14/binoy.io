69154467fbf9633c43b1114b6d2631e68058680d
Binoy <binoypatel14@gmail.com>
2023-06-15 23:33:26 -0400

    fix: fix storybook upgrade issue and move to pnpm

--- libs/ui/.storybook/preview.js
+++ libs/ui/.storybook/preview.js
@@ -1,9 +1,1 @@
 import "./tailwind.css";
-
-import { RouterContext } from "next/dist/shared/lib/router-context";
-
-export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider,
-  },
-};
