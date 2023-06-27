65e651c8ec7d4bdfa45681d1cee6c6a455bccfed
Binoy Patel <me@binoy.io>
2023-06-26 22:19:24 -0400

    fix: use next/font for optimized fonts (#42)

--- apps/home/pages/_app.tsx
+++ apps/home/pages/_app.tsx
@@ -2,14 +2,27 @@ import "../public/global.css";
 
 import { Analytics } from "@vercel/analytics/react";
 import { NextSeo } from "next-seo";
+import { Fira_Code } from "next/font/google";
 
 import { Layout } from "../components/Layout";
 
 import type { AppProps } from "next/app";
 
+const FiraCode = Fira_Code({
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin-ext"],
+  variable: "--font-fira-code",
+});
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        :root {
+          --font-fira-code: ${FiraCode.style.fontFamily};
+        }
+      `}</style>
       <Layout>
         <NextSeo
           title="Binoy Patel"
--- apps/home/pages/_document.tsx
+++ apps/home/pages/_document.tsx
@@ -3,15 +3,8 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+      <Html lang="en">
+        <Head />
         <body className="bg-black text-white">
           <Main />
           <NextScript />
