233bc212d83247a533e2e4985b888d2d3eca5e84
Binoy <binoypatel14@gmail.com>
2023-12-13 23:12:09 -0500

    chore(deps): Upgrade Nx and Next

--- libs/ui/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-const rootMain = require("../../../.storybook/main");
-
-module.exports = {
-  ...rootMain,
-  core: { ...rootMain.core },
-  stories: [...rootMain.stories, "../src/lib/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", ...rootMain.addons, "@nx/react/plugins/storybook"],
-};
