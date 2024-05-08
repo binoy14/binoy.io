4fd51ad92ee6a882ee0e3e1ecf88a6cf61635cf2
Binoy <binoypatel14@gmail.com>
2024-05-07 21:20:35 -0400

    feat: add SEO data

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -24,7 +24,25 @@
       link: 'contact',
     },
   ];
+
+  const seoData = {
+    title: 'Binoy Patel',
+    description:
+      'Tangential is a consulting company that helps companies with Web and Mobile Applications.',
+    canonical: 'https://www.binoy.io',
+  };
 </script>
+
+<svelte:head>
+  <title>{seoData.title}</title>
+  <meta property="og:type" content="website" />
+  <link rel="canonical" href={seoData.canonical} />
+  <meta name="description" content={seoData.description} />
+  <meta property="og:title" content={seoData.title} />
+  <meta property="og:description" content={seoData.description} />
+  <meta property="og:url" content={seoData.canonical} />
+  <meta property="og:site_name" content={seoData.title} />
+</svelte:head>
 
 <Navigation {links} title="Binoy Patel" />
 
