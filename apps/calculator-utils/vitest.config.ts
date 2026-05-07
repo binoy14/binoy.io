f9b7f6b994cdda25ca09a035349d3e5ec49069a6
Binoy Patel <me@binoy.io>
2026-05-06 20:56:09 -0400

    chore: replace jsdom with happy-dom for tests (#338)

--- apps/calculator-utils/vitest.config.ts
+++ apps/calculator-utils/vitest.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
   },
 });
